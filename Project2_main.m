979ab817224d7ba04fbee477af2a24261959445a
ang0057 <ang0057@auburn.edu>
2026-03-05 12:48:28 -0600

    Figured out ftp, so I'm making a commit here after gathering some data

--- Project2_main.m
+++ Project2_main.m
@@ -3,3 +3,18 @@ clear;
 clc;
 
 % Project 2 initial commit
+
+% figured out ftp, here is the 10.7cm data for my birthday (8/15/03)
+% :Product: Daily Solar Data         2003_DSD.txt
+% #
+% #  Prepared by the U.S. Dept. of Commerce, NOAA, Space Environment Center.
+% #  Please send comments and suggestions to sec.webmaster@noaa.gov
+% #
+% #                           2003 Daily Solar Data
+% #
+% #                         Sunspot       Stanford GOES8
+% #           Radio  SESC     Area          Solar  X-Ray  ------ Flares ------
+% #           Flux  Sunspot  10E-6   New     Mean  Bkgd    X-Ray      Optical
+% #  Date     10.7cm Number  Hemis. Regions Field  Flux   C  M  X  S  1  2  3
+% #---------------------------------------------------------------------------
+% 2003 08 15  131     86      540      1    -999   B6.8   9  0  0  5  0  0  0
